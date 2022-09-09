5d791f8e1318301d0a6283288fcf6a1f358ec0e9
Barok Dagim <61631544+BarokDG@users.noreply.github.com>
2022-09-09 23:46:32 +0300

    Time: 120 ms (9.22%), Space: 15.1 MB (12.05%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,12 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        slis = list(s)
-        tlis = list(t)
+        slis, tlis = list(s), list(t)
         
         slis.sort()
         tlis.sort()
         
-        sd = [i for i in slis]
-        td = [i for i in tlis]
-        
-        return sd == td
+        return slis == tlis
