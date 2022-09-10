c1fa5b41da45d059aa9f15b9cecabf26674d1dd6
Barok Dagim <61631544+BarokDG@users.noreply.github.com>
2022-09-10 12:22:06 +0300

    Time: 2282 ms (28.76%), Space: 14.8 MB (99.57%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,5 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if nums.count(target - nums[i]) and nums.index(target - nums[i]) != i:               
+                return [i, nums.index(target - nums[i])]
