3c2da930568a5fc1df6710ba81a7482d739785db
Barok Dagim <61631544+BarokDG@users.noreply.github.com>
2022-09-18 21:26:00 +0300

    Time: 503 ms (12.84%), Space: 14.2 MB (60.52%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        results = []
+        
+        for i in range(len(l)):
+            temp = nums.copy()[l[i]:r[i] + 1]
+            temp.sort()
+            
+            change = temp[1] - temp[0]
+            
+            skipOuter = False
+            
+            for i in range(1, len(temp) - 1):
+                if temp[i + 1] - temp[i] != change:
+                    results.append(False)
+                    skipOuter = True
+                    break
+                
+            if skipOuter == False:
+                results.append(True)
+            
+        return results
+        
