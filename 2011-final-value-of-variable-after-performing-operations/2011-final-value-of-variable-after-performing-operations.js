1f4d031a9a3d701031a822dc94d6b77755f971b5
Barok Dagim <61631544+BarokDG@users.noreply.github.com>
2023-01-14 13:55:04 +0300

    Time: 65 ms (88.07%), Space: 42.1 MB (83.39%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -3,18 +3,8 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-//     let x = 0
-    
-//     for (let operation of operations) {
-//         if (operation === "--X" || operation === "X--") {
-//             x--
-//         } else {
-//             x++
-//         }
-//     }
-    
     return operations.reduce((x, operation) => {
-        if (operation.includes("-")) {
+        if (operation[1] === "-") {
             x--
         } else {
             x++
